49cbc2af1f052970fa318bc7ac924bfb8e9161bf
Fellipe Anaia Duarte Teixeira <fellipeanaia@gmail.com>
2022-10-15 21:28:16 -0300

    two

---
+++ lib/view/store_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class StorePage extends StatefulWidget {
+  const StorePage({Key? key}) : super(key: key);
+
+  @override
+  State<StorePage> createState() => _StorePageState();
+}
+
+class _StorePageState extends State<StorePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
